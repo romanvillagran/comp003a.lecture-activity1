5535a4d36f546beb60cac75da88aa9682b92b98e
romanvillagran <romanvillagran386@gmail.com>
2025-01-15 19:58:40 -0800

    Add project files.

---
+++ comp003a.lecture,activity1/Program.cs
@@ -0,0 +1,13 @@
+﻿// namesoace level
+namespace comp003a.lecture_activity1
+{
+    // class level
+    internal class Program
+    {
+        //main level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
